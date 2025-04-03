6d443238bb1e063f58422652892460fa9c46715e
SteveNadeau <you_tuber@live.ca>
2025-04-03 12:56:15 -0400

    this is my work as of April 3rd

---
+++ view/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $pagetitle ?></title>
+   
+</head>
+
+<body>
+<header>
+	<nav>
+		<a href="login_register.php">Home</a> | <?php echo $menu
+		
+		if(isset($_SESSION['username']))
+    { ?>
+    <nav>    
+        <a href="session_example.php">Home</a> |
+        <a href="session_example.php?action=forget">Forget Me</a> |     
+        <a href="session_example.php?action=changeform">Change Info</a>    
+    </nav>
+    <?php }    else { ?>
+    <nav>    
+        <a href="session_example.php">Home</a> | 
+        <a href="session_example.php?action=forget">Forget Me</a> 
+    </nav>
+    
+    <?php } ?>
+		
+		
+	</nav>
+</header>
+	
